9ac8d9e0c989bad1fde5e9d15c4bda020f4b1f18
bilarslan <bilarslan@windowslive.com>
2020-09-12 14:52:30 +0300

    card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,27 +10,28 @@ function Card({ data }) {
             </div>
             <div className='user-info'>
                 <div className='info'>
-                    <h2>{data.name}</h2>
-                    {data.bio && <p>{data.bio}</p>}
+                    <h2>{data.name != null && data.name}</h2>
+                    {data.bio != null&& <p>{data.bio}</p>}
                 </div>
 
                 {data.company && <p><i className='fas fa-building'></i>{data.company}</p>}
                 {data.location && <p><i className='fas fa-map-marker-alt' />{data.location}</p>}
                 <div className='links-info'>
                     <ul>
-                        {data.email && <li><i className='fas fa-envelope'></i></li>}
-                        <li><i className='fab fa-github'></i></li>
-                        {data.twitter_username != null && <li><i className='fab fa-twitter'></i></li>}
-                        {data.linkedin_url != null && <li><i className='fab fa-linkedin-in'></i></li>}
+                        {data.email && <li><a href={`mailto:${data.email}`} className='nav-link' target='_blank'><i className='fas fa-envelope' /></a></li>}
+                        <li><a href={data.html_url} className='nav-link' target='_blank'><i className='fab fa-github' /></a></li>
+                        {data.twitter_username != null && <li><a href={`https://twitter.com/${data.twitter_username}`} className='nav-link' target='_blank'><i className='fab fa-twitter' /></a></li>}
+                        {data.linkedin_url != null && <li><a href={data.linkedin_url} className='nav-link' target='_blank'><i className='fab fa-linkedin-in' /></a></li>}
                     </ul>
                 </div>
-                <div className='git-info'>
-                    <ul>
-                        <li><i className='fas fa-user-friends' />{`${data.followers} Followers · ${data.following} Following`}</li>
-                        <li><i className='fas fa-book' />{`${data.public_repos} Repos`}</li>
-                    </ul>
-                </div>
-
+                {data.showRepoStatus === true &&
+                    <div className='git-info'>
+                        <ul>
+                            <li><i className='fas fa-user-friends' />{`${data.followers} Followers · ${data.following} Following`}</li>
+                            <li><i className='fas fa-book' />{`${data.public_repos} Repos`}</li>
+                        </ul>
+                    </div>
+                }
             </div>
         </div>
     )
