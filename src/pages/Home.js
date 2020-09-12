2e5c74283e1f3d4eab35e82796e3ff1cdb95661e
bilarslan <bilarslan@windowslive.com>
2020-09-12 14:52:51 +0300

    homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,31 +3,38 @@ import axios from 'axios';
 
 import Card from '../components/Card';
 
-const APIURL = 'https://api.github.com/users/YOUR_USER_NAME';
+const APIURL = 'https://api.github.com/users/YOUR_GIHUB_USERNAME';
 
 function Home() {
 
     const [data, setData] = useState(null);
+    const [error, setError] = useState(false);
 
     const extraData = {
-        linkedin_url: null, //'https://www.linkedin.com/in/bilarslan/'
+        showRepoStatus: true,
+        email: null, //'YOUR_EMAIL@ADDRESS',
+        linkedin_url: null, //'https://www.linkedin.com/in/YOUR_LINKEDIN_USERNAME/',
     };
 
     useEffect(() => {
         axios.get(APIURL)
             .then(result => {
-                console.log(result.data);
+                //console.log(result.data)
                 setData(Object.assign(result.data, extraData));
+                document.title = `HOMEPAGE | ${result.data.name}`;
             })
             .catch(error => {
-                console.log('erorrr');
+                setError(true);
+                console.error('ERROR!', error?.response?.data)
             });
     }, []);
 
     return (
         <>
             {data == null &&
-                <i className="fas fa-spinner"></i>
+                <div style={{ color: '#FFF', fontSize: '2rem' }}>
+                    <i className={error === true ? 'fas fa-bug' : 'fas fa-spinner fa-spin'}></i>
+                </div>
             }
             {data != null &&
                 <Card data={data} />
